c6d5836762ae375e95f32b20523b43c0b6048866
Chris Hubbard <chris@thehubbards.org>
2020-02-21 13:52:17 -0500

    add DIR to download_secrets.h

--- download_secrets.sh
+++ download_secrets.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 mkdir -p "${DIR}/appbuilder/secrets"
 aws s3 sync s3://sil-stg-aps-secrets/travis/build "${DIR}/appbuilder/secrets"

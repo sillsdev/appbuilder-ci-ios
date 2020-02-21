274d46ae65d83ab46acd8683803a84ea10f962ec
Chris Hubbard <chris@thehubbards.org>
2020-02-21 09:21:29 -0500

    Use full paths when referencing files with security app

--- resign_asset.sh
+++ resign_asset.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
-set -x
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # import certificate
-mkdir -p appbuilder/secrets
-aws s3 sync s3://sil-stg-aps-secrets/travis/build appbuilder/secrets
-ls -l appbuilder/secrets
-security create-keychain -p $KEYCHAIN_PASSWORD appbuilder/appbuilder.keychain
-security import appbuilder/secrets/Certificates.p12 -t agg -k appbuilder/appbuilder.keychain -P $CERTIFICATE_PASSWORD -A
+mkdir -p "${DIR}/appbuilder/secrets"
+aws s3 sync s3://sil-stg-aps-secrets/travis/build "${DIR}/appbuilder/secrets"
+security create-keychain -p $KEYCHAIN_PASSWORD "${DIR}/appbuilder/appbuilder.keychain"
+security import "${DIR}/appbuilder/secrets/Certificates.p12" -t agg -k "${DIR}/appbuilder/appbuilder.keychain" -P $CERTIFICATE_PASSWORD -A
 
 # set keychain
-security list-keychains -s appbuilder/appbuilder.keychain
-security default-keychain -s appbuilder/appbuilder.keychain
-security unlock-keychain -p \"${KEYCHAIN_PASSWORD}\" appbuilder/appbuilder.keychain
+security list-keychains -s "${DIR}/appbuilder/appbuilder.keychain"
+security default-keychain -s "${DIR}/appbuilder/appbuilder.keychain"
+security unlock-keychain -p "${KEYCHAIN_PASSWORD}" "${DIR}/appbuilder/appbuilder.keychain"
 
 # download asset
 mkdir asset
 aws s3 sync s3://cth-dev-aps-artifacts/travis asset
 
 # resign app
-./resign.sh asset/WEB-1.4.ipa "iPhone Distribution: Chris Hubbard (5P99T946M7)" -p appbuilder/secrets/app.mobileprovision asset/WEB-1.4-resigned.ipa
+./resign.sh asset/WEB-1.4.ipa "iPhone Distribution: Chris Hubbard (5P99T946M7)" -p "${DIR}/appbuilder/secrets/app.mobileprovision" asset/WEB-1.4-resigned.ipa
 
 # upload result
 aws s3 sync asset s3://cth-dev-aps-artifacts/travis

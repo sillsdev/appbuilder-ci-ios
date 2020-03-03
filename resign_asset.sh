2980c66cb0c18175e276949809f4671bf945bfd4
Chris Hubbard <chris@thehubbards.org>
2020-03-03 13:28:23 -0500

    sign appstore and adhoc

--- resign_asset.sh
+++ resign_asset.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -e
-set -x
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # import certificate
@@ -11,13 +10,13 @@ security import "${DIR}/appbuilder/secrets/Certificates.p12" -t agg -k "${DIR}/a
 security list-keychains -s "${DIR}/appbuilder/appbuilder.keychain"
 security default-keychain -s "${DIR}/appbuilder/appbuilder.keychain"
 security unlock-keychain -p "${KEYCHAIN_PASSWORD}" "${DIR}/appbuilder/appbuilder.keychain"
-security find-identity -p codesigning -v
 SIGNING_IDENTITY=$(security find-identity -p codesigning -v | grep "^  \d)" | cut -d\" -f2)
-echo "SIGNING_IDENTITY=${SIGNING_IDENTITY}"
 
 # resign app
 mkdir -p "${DIR}/result"
 EXT="${APPBUILDER_ASSET##*.}"
-APPBUILDER_RESULT="${APPBUILDER_ASSET%.*}-resigned.${EXT}"
-./resign.sh "${DIR}/asset/${APPBUILDER_ASSET}" "${SIGNING_IDENTITY}" -p "${DIR}/appbuilder/secrets/app.mobileprovision" "${DIR}/result/${APPBUILDER_RESULT}"
+APPBUILDER_APPSTORE_RESULT="${APPBUILDER_ASSET%.*}-appstore.${EXT}"
+APPBUILDER_ADHOC_RESULT="${APPBUILDER_ASSET%.*}-adhoc.${EXT}"
+./resign.sh "${DIR}/asset/${APPBUILDER_ASSET}" "${SIGNING_IDENTITY}" -p "${DIR}/appbuilder/secrets/appstore.mobileprovision" "${DIR}/result/${APPBUILDER_APPSTORE_RESULT}"
+./resign.sh "${DIR}/asset/${APPBUILDER_ASSET}" "${SIGNING_IDENTITY}" -p "${DIR}/appbuilder/secrets/adhoc.mobileprovision" "${DIR}/result/${APPBUILDER_ADHOC_RESULT}"
 

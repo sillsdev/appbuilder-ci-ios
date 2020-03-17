53b00abd5da0e1eea9d242dd8c9c4ecfc6d5707b
Chris Hubbard <chris@thehubbards.org>
2020-03-16 22:19:09 -0400

    update builder paths

--- builder.sh
+++ builder.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-mkdir builder && cd builder && unzip ../asset/builder.zip
-mkdir -p builder/HTML && cd builder/HTML && unzip ../../asset/html.zip
+(mkdir builder && cd builder && unzip "${DIR}/asset/builder.zip")
+(mkdir -p builder/HTML && cd builder/HTML && unzip "${DIR}/asset/html.zip")
 find builder -print
